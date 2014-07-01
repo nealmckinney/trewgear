ec43b408ff2cd6e7fedfafd818be43db1f85a0be
Neal Mckinney <neal_mckinney@yahoo.com>
2014-07-01 11:23:58 -0700

    web fonts

--- meta.php
+++ meta.php
@@ -10,6 +10,8 @@
 <meta name="google-site-verification" content="MpahA5d53hTF03x47XvpzouRgbll4N3MOQd-lhdEXqA" />
 <meta name="msvalidate.01" content="424C1652D2045E8F0E2605760EA90F53" />
 
+<link rel="stylesheet" type="text/css" href="//cloud.typography.com/7607112/719304/css/fonts.css" />
+
 <link href="resources/css/styles_2014.css" rel="stylesheet" type="text/css"/>
 <link href="/resources/css/styles-responsive.css" rel="stylesheet" type="text/css"/>
 <script src="resources/js/vendor/modernizr-2.6.2.min.js"></script>
